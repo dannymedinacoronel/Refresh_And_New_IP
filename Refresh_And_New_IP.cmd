92efd25cdfce99f7f0b9fa7277ffae26b36f0165
Danny Medina Coronel <39246816+dannymedinacoronel@users.noreply.github.com>
2021-11-16 14:47:11 +0100

    Add files via upload

---
+++ Refresh_And_New_IP.cmd
@@ -0,0 +1,37 @@
+@echo off 
+
+
+
+:choice 
+cls
+echo Choose: 
+echo [C] Set DHCP
+echo [Z] Quit 
+echo. 
+
+SET /P C=[C]? 
+
+for %%? in (C) do if /I "%C%"=="%%?" goto C
+for %%? in (Z) do if /I "%C%"=="%%?" goto Z
+goto choice 
+
+
+:C 
+@ECHO OFF 
+ECHO Resetting IP Address and Subnet Mask For DHCP 
+netsh int ip set address name = "LAN" source = dhcp
+
+ipconfig /release
+ipconfig /renew
+
+ECHO aqui esta la nueva configuracion de red %computername%: 
+
+netsh int ip show config
+
+pause 
+goto choice
+
+
+:Z
+exit
+:end
